6e79e51b73b67c8e3dadfe65d8a1c6b66164ccce
Berna <bernay@Bilgisayarim.local>
2020-09-07 20:08:03 -0400

    Week 1 Homework mvp

---
+++ tutorial1_intro/main.js
@@ -0,0 +1,31 @@
+d3.csv("../data/BooksRead.csv").then(data=> {
+    
+    console.log("data",data)
+    
+    const table= d3.select("#d3-table");
+    const columns = ["Year", "Month", "Number of Pages", "Read Count"]
+    
+    const column_headers=table
+        .selectAll("th")
+        .data(data.columns)
+        .join("th")
+        .text(function (d) { return d })
+    
+    const body = table.append('tbody')
+
+    const rows = body.selectAll(".row")
+        .data(data)
+        .join('tr')
+        .attr('class', 'row')
+ 
+    const cells = rows.selectAll('td')
+        .data(function(row) {
+            return columns.map(function (column) {
+                return { column: column, value: row[column] }
+                })
+            })
+        .join('td')
+        .text(function (d) { return d.value })
+        .attr("class", d=> + d["Number of Pages"] >5 ? "high" :null)
+
+});
