9ae79ac12aec141d54c9756c012e20c1ddce14a8
Berna <bernay@Bilgisayarim.local>
2020-09-08 13:20:26 -0400

    Updates

--- tutorial1_intro/main.js
+++ tutorial1_intro/main.js
@@ -48,7 +48,10 @@ d3.csv("../data/BooksRead.csv").then(data=> {
         .data(d => Object.values(d))
         .join('td')
         .text(d => d)
-
+        .attr("color",function(d){
+            return "rgb(0,0,"+ Math.round(d.Books*10)+")";
+        });
+});
 // Attempts to update rows       
 /* thead
         .append('tr')
@@ -59,4 +62,26 @@ d3.csv("../data/BooksRead.csv").then(data=> {
     if (d.Books>8) {return "wow"};
     })) */
 
-}); 
+ /*  var w=500;
+    var h=500;
+    var barPadding=2;
+    var Padding=20;
+
+    const svg=d3.select("body").append("svg")        //Bar chart
+        .attr("width",w)
+        .attr("height",h);
+
+    svg.selectAll("rect")
+        .data(data)
+        .join("rect")
+        .attr("x",200)
+        .attr("y",function(d){
+        return h/d;
+        })
+        .attr("width",w/data.length-barPadding)
+        .attr("height",function(d){
+        return d*4;
+        })
+        .attr("fill",function(d){
+        return "rgb(0,0,"+ Math.round(d*10)+")";
+        });   */
