83be25ac98632a47c13c90e9acd0366b50afa2ed
Berna <bernay@Bilgisayarim.local>
2020-10-03 17:37:45 -0400

    Updates - but will start again

--- tutorial3_distributions/mainWEForum.js
+++ tutorial3_distributions/mainWEForum.js
@@ -1,7 +1,7 @@
 /* CONSTANTS AND GLOBALS */
-const width = window.innerWidth * 0.7,
+const width = window.innerWidth * 0.9,
   height = window.innerHeight * 0.9,
-  margin = { top: 20, bottom: 50, left: 60, right: 40 },
+  margin = { top: 20, bottom: 50, left: 200, right: 40 },
   radius = 5;
 
   console.log('width',width)
@@ -10,21 +10,23 @@ const width = window.innerWidth * 0.7,
 let svg;
 let xScale;
 let yScale;
-
+let colors;
 
 /* APPLICATION STATE */
 let state = {
   data: [],
-  selectedParty: "All" // + YOUR FILTER SELECTION
+  selectedQuestion: "Show All" ,// + YOUR FILTER SELECTION
 };
+console.log(state)
 
 /* LOAD DATA */
-d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
+d3.csv("../data/WEF_COVID_19_Risks_Outlook.csv", d3.autoType).then(raw_data => {
   // + SET YOUR DATA PATH
   console.log("raw_data", raw_data);
   state.data = raw_data; 
   init();
 });
+
  console.log("margin left", margin.left)
  console.log("margin right",width-margin.right)
 /* INITIALIZING FUNCTION */
@@ -32,38 +34,63 @@ d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
 function init() {
   // + SCALES
   xScale=d3.scaleLinear()
-    .domain(d3.extent(state.data, d => d.ideology_rating))
+    .domain(d3.extent(state.data, d => d.Risk_Rating))
     .range([margin.left, width-margin.right])
 
-  yScale=d3.scaleLinear()
+  console.log("xScale - 20",d3.extent(xScale(20)))
+
+  yScale=d3.scaleBand()
 //    .domain([0,100])
-    .domain(d3.extent(state.data, d => d.environmental_rating))
+    .domain(state.data.map(d => d.Risk_Summary))
     .range([height-margin.bottom,margin.top])
 
-  // + AXES
+
+colors = d3.scaleOrdinal()
+    .domain(state.data.map(d => d.Risk_Level))
+    .range(["#3288BD", "#66C2A5", "#ABDDA4"]);
+
+    console.log("scaleOrdinal",colors("High"),colors("Medium"),colors("Low"))
+// + AXES
 
 const xAxis=d3.axisBottom(xScale)
-.ticks(5)
+.ticks(10)
 
 const yAxis=d3.axisLeft(yScale)
-.ticks(5);
-
 
   // + UI ELEMENT SETUP
 
-  const selectElement = d3.select("#dropdown").on("change", function() {
+  const selectCategory = d3.select("#dropdown1").on("change", function() {
     // `this` === the selectElement
     // 'this.value' holds the dropdown value a user just selected
 
-    state.selectedParty = this.value;
+    state.selectedCategory = this.value;
     console.log("new value is", this.value);
     draw(); // re-draw the graph based on this new selection
   });
 
+  const selectQuestion = d3.select("#dropdown2").on("change", function() {
+    // `this` === the selectElement
+    // 'this.value' holds the dropdown value a user just selected
+
+    state.selectedQuestion = this.value;
+    console.log("new value is", this.value);
+    draw(); // re-draw the graph based on this new selection
+  });
+
+const selectRatingLevel = d3.select("#dropdown3").on("change", function() {
+  // `this` === the selectElement
+  // 'this.value' holds the dropdown value a user just selected
+
+  state.selectedRatingLevel = this.value;
+  console.log("new value is", this.value);
+  draw(); // re-draw the graph based on this new selection
+});
+
   // add in dropdown options from the unique values in the data
-  selectElement
+
+  selectQuestion
     .selectAll("option")
-    .data(["All", "D", "R", "I"]) // + ADD UNIQUE VALUES
+    .data(["Show All","Greatest concern for the world","Most likely fallout for the world","Most worrisome for your company"]) // + ADD UNIQUE VALUES
     .join("option")
     .attr("value", d => d)
     .text(d => d);
@@ -77,15 +104,6 @@ const yAxis=d3.axisLeft(yScale)
 
   // + CALL AXES
 
-/*   svg.append("g")
-  .attr("class","x-axis")
-  .attr("transform","translate(0,"+(width)+")")
-  .call(xAxis);   
-
-  svg.append("g")
-  .attr("class","y-axis")
-  .attr("transform","translate("+(height)+",0)")
-  .call(yAxis); */
   svg
   .append("g")
   .attr("class", "axis x-axis")
@@ -95,7 +113,7 @@ const yAxis=d3.axisLeft(yScale)
   .attr("class", "axis-label")
   .attr("x", "50%")
   .attr("dy", "3em")
-  .text("Ideology Rating");
+  .text("Risk Rating");
 
   svg
   .append("g")
@@ -107,7 +125,7 @@ const yAxis=d3.axisLeft(yScale)
   .attr("y", "50%")
   .attr("dx", "-3em")
   .attr("writing-mode", "vertical-rl")
-  .text("Environmental Rating");
+  .text("Risk Name");
 
 
   draw(); // calls the draw function
@@ -118,17 +136,18 @@ const yAxis=d3.axisLeft(yScale)
 
 function draw() {
   
-  // + FILTER DATA BASED ON STATE
+  // + FILTER DATA BASED ON CATEGORY, QUESTION, RATING LEVEL
+
   let filteredData = state.data;
   // if there is a selectedParty, filter the data before mapping it to our elements
-  if (state.selectedParty !== "All") {
-    filteredData = state.data.filter(d => d.party === state.selectedParty);
-  }
 
+    filteredData = state.data.filter(d => d.Question === state.selectedQuestion);
 
+ 
+  console.log(filteredData)
 const dot = svg
    .selectAll("circle")
-   .data(filteredData, d => d.name)
+   .data(filteredData, d => d.Risk_Summary)
     .join(
       enter =>
         // enter selections -- all data elements that don't have a `.dot` element attached to them yet
@@ -137,20 +156,23 @@ const dot = svg
           .attr("class", "dot") // Note: this is important so we can identify it in future updates
           .attr("stroke", "lightgrey")
           .attr("opacity", 0.5)
-          .attr("fill", d => {
-            if (d.party === "D") return "blue";
+ //         .attr("fill", d=>colors(d.Risk_Level))
+/*           .attr("fill", d => {
+            if (d.Risk_Level === "High") return "blue";
             else if (d.party === "R") return "red";
+
             else return "purple";
-          })
+          }) */
           .attr("r", radius)
-          .attr("cy", d => yScale(d.environmental_rating))
+          .attr("cy", d => yScale(d.Risk_Summary))
+          .attr("cx", d => xScale(d.Risk_Rating)) // initial value - to be transitioned
           .attr("cx", d => margin.left) // initial value - to be transitioned
           .call(enter =>
             enter
               .transition() // initialize transition
-              .delay(d => 500 * d.ideology_rating) // delay on each element
+              .delay(d => 500 * d.Risk_Rating) // delay on each element
               .duration(500) // duration 500ms
-              .attr("cx", d => xScale(d.ideology_rating))
+              .attr("cx", d => xScale(d.Risk_Rating))
           ),
       update =>
         update.call(update =>
@@ -168,7 +190,7 @@ const dot = svg
           // exit selections -- all the `.dot` element that no longer match to HTML elements
           exit
             .transition()
-            .delay(d => 50 * d.ideology_rating)
+            .delay(d => 50 * d.Risk_Rating)
             .duration(500)
             .attr("cx", width)
             .remove()
